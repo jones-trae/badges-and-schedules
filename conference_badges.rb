94838da613116f8e2f63fcaa52bf0867b8c2d65b
Valton Jones <jones.trae@gmail.com>
2021-03-01 05:03:00 -0800

    commit tree ff0d6ee02a0392963bc1e6e391d349af1fd2029d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(people)
+    people.map do | greeting |
+        "Hello, my name is #{greeting}."
+    end
+end
+
+def assign_rooms(people)
+    people.map.with_index(1) do | speaker, room |
+    "Hello, #{speaker}! You'll be assigned to room #{room}!"  
+    end  
+end
+
+def printer(people)
+    batch_badge_creator(people).each do | badge |
+        puts badge
+    end
+
+    assign_rooms(people).each do | room |
+        puts room
+    end
+
+end
